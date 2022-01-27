c2cff7e4d6ea1ff4b531db2db0d3987af35dd19e
Eric Brown <ericbrown589@yahoo.com>
2022-01-26 22:06:26 -0600

    Initialize Spring Boot project

---
+++ src/main/java/com/randomcharacter/randomcharacter/RandomCharacterApplication.java
@@ -0,0 +1,13 @@
+package com.randomcharacter.randomcharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomCharacterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RandomCharacterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/randomcharacter/randomcharacter/RandomCharacterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomcharacter.randomcharacter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomCharacterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

9ebace488b40f42405982b6c6613131d7aa1c72f
Eric Brown <ericbrown589@yahoo.com>
2022-02-03 11:46:28 -0600

    update to test deployment

--- src/main/java/com/randomcharacter/randomcharacter/controller/CharacterController.java
+++ src/main/java/com/randomcharacter/randomcharacter/controller/CharacterController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://random-character.herokuapp.com/")
 public class CharacterController {
 
     private CharacterService characterService;
